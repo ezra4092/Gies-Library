bee13a20b3b4dd7c664809f6e8215806a5958946
alyyaps1374-cell <alyyaps1374@gmail.com>
2026-04-15 14:11:44 +0700

    Alyya-J0403251109
    Update: Data Buku

--- config.py
+++ config.py
@@ -14,24 +14,3 @@ USER_FILE = os.path.join(BASE_DIR, "data", "user.json")
 # Admin Credentials (Hardcoded)
 ADMIN_USN = "admin"
 ADMIN_PW = "admin123"
-
-# Skin Type Options
-SKIN_TYPES = {
-    "1": "Oily",
-    "2": "Dry",
-    "3": "Combination",
-    "4": "Sensitive",
-    "5": "Normal"
-}
-
-# BMI Categories
-BMI_CATEGORIES = {
-    "1": {"name": "Underweight", "range": "< 18.5"},
-    "2": {"name": "Normal", "range": "18.5 - 24.9"},
-    "3": {"name": "Overweight", "range": "25 - 29.9"},
-    "4": {"name": "Obese", "range": ">= 30"}
-}
-
-# PDF Colors
-COLOR_SKINCARE = '#2E86AB'  # Blue
-COLOR_WORKOUT = '#F18F01'   # Orange
